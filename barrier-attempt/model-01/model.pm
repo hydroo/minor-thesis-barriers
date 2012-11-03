8e2313516995d18e6c4bff498f581335deb5e330
Ronny Brendel <ronnybrendel@gmail.com>
2012-11-03 13:33:14 +0100

    mini refactoring:  * rename s_* to l_* to make it a bit clearer that we are talking about process graphs  * full = me_1 + me_2 instead of a hardcoded number

--- barrier-attempt/model-01/model.pm
+++ barrier-attempt/model-01/model.pm
@@ -4,7 +4,7 @@ dtmc
 const me_1 = 1;
 const me_2 = 2;
 
-const full = 3;
+const full = me_1 + me_2;
 
 const min_invalid = -1;
 const max_invalid = full + 1;
@@ -22,62 +22,62 @@ label "invalid_barrier" =
 
 
 module process_1
-	s_1 : [0..17] init 0;
+	l_1 : [0..17] init 0;
 	cp_1 : [min_invalid..max_invalid] init 0;
 
-	[] s_1=0 -> (s_1'=1) & (entry'=1);
+	[] l_1=0 -> (l_1'=1) & (entry'=1);
 
-	[] s_1=1 -> (s_1'=2) & (cp_1'=entry);
+	[] l_1=1 -> (l_1'=2) & (cp_1'=entry);
 
 	// emulate bit manipulation: (cp_1&me) != 0
-	[] s_1=2 & mod(floor(cp_1/me_1),2)=1 -> (s_1'=4);
+	[] l_1=2 & mod(floor(cp_1/me_1),2)=1 -> (l_1'=4);
 	// emulate bit manipulation: (cp_1&me) == 0
-	[] s_1=2 & mod(floor(cp_1/me_1),2)=0 -> (s_1'=3);
+	[] l_1=2 & mod(floor(cp_1/me_1),2)=0 -> (l_1'=3);
 	// prism wouldn't accept + because it might exceed the bounds ... therefore use min, max
 	// emulate bit manipulation: cp_1 |= me_1
-	[] s_1=3 -> (s_1'=4) & (cp_1'=min(max_invalid,max(cp_1+me_1, min_invalid)));
+	[] l_1=3 -> (l_1'=4) & (cp_1'=min(max_invalid,max(cp_1+me_1, min_invalid)));
 
-	[] s_1=4 -> (s_1'=5) & (entry'=cp_1);
+	[] l_1=4 -> (l_1'=5) & (entry'=cp_1);
 
 	// diamon/nondeterminis for cp_1 != full & left = false
-	[] s_1=5 & (  cp_1 != full &   left = false ) -> 0.5:(s_1'=6) + 0.5:(s_1'=7);
-	[] s_1=5 & (  cp_1 != full & !(left = false)) -> 0.5:(s_1'=6) + 0.5:(s_1'=8);
-	[] s_1=5 & (!(cp_1 != full) &  left = false ) -> 0.5:(s_1'=7) + 0.5:(s_1'=8);
-	[] s_1=6 &   left = false  -> (s_1'=1);
-	[] s_1=6 & !(left = false) -> (s_1'=8);
-	[] s_1=7 &   cp_1 != full  -> (s_1'=1);
-	[] s_1=7 & !(cp_1 != full) -> (s_1'=8);
+	[] l_1=5 & (  cp_1 != full &   left = false ) -> 0.5:(l_1'=6) + 0.5:(l_1'=7);
+	[] l_1=5 & (  cp_1 != full & !(left = false)) -> 0.5:(l_1'=6) + 0.5:(l_1'=8);
+	[] l_1=5 & (!(cp_1 != full) &  left = false ) -> 0.5:(l_1'=7) + 0.5:(l_1'=8);
+	[] l_1=6 &   left = false  -> (l_1'=1);
+	[] l_1=6 & !(left = false) -> (l_1'=8);
+	[] l_1=7 &   cp_1 != full  -> (l_1'=1);
+	[] l_1=7 & !(cp_1 != full) -> (l_1'=8);
 
-	[] s_1=5 &  !(cp_1 != full) & !(left = false) -> (s_1'=8);
-	[] s_1=8 -> (s_1'=9) & (left'=true);
+	[] l_1=5 &  !(cp_1 != full) & !(left = false) -> (l_1'=8);
+	[] l_1=8 -> (l_1'=9) & (left'=true);
 
-	[] s_1=9 -> (s_1'=10) & (exit'=1);
+	[] l_1=9 -> (l_1'=10) & (exit'=1);
 
-	[] s_1=10 -> (s_1'=11) & (cp_1'=exit);
+	[] l_1=10 -> (l_1'=11) & (cp_1'=exit);
 
-	[] s_1=11 & mod(floor(cp_1/me_1),2)=1 -> (s_1'=14);
-	[] s_1=11 & mod(floor(cp_1/me_1),2)=0 -> (s_1'=12);
-	[] s_1=12 -> (s_1'=13) & (cp_1'=min(max_invalid,max(cp_1+me_1, min_invalid)));
+	[] l_1=11 & mod(floor(cp_1/me_1),2)=1 -> (l_1'=14);
+	[] l_1=11 & mod(floor(cp_1/me_1),2)=0 -> (l_1'=12);
+	[] l_1=12 -> (l_1'=13) & (cp_1'=min(max_invalid,max(cp_1+me_1, min_invalid)));
 
-	[] s_1=13 -> (s_1'=14) & (exit'=cp_1);
+	[] l_1=13 -> (l_1'=14) & (exit'=cp_1);
 
 	// diamond/nondeterminism for cp_1 != full & left = true
-	[] s_1=14 & (  cp_1 != full  &   left = true ) -> 0.5:(s_1'=15) + 0.5:(s_1'=16);
-	[] s_1=14 & (  cp_1 != full  & !(left = true)) -> 0.5:(s_1'=15) + 0.5:(s_1'=17);
-	[] s_1=14 & (!(cp_1 != full) &   left = true ) -> 0.5:(s_1'=16) + 0.5:(s_1'=17);
-	[] s_1=15 &   left = true  -> (s_1'=10);
-	[] s_1=15 & !(left = true) -> (s_1'=17);
-	[] s_1=16 &   cp_1 != full  -> (s_1'=10);
-	[] s_1=16 & !(cp_1 != full) -> (s_1'=17);
+	[] l_1=14 & (  cp_1 != full  &   left = true ) -> 0.5:(l_1'=15) + 0.5:(l_1'=16);
+	[] l_1=14 & (  cp_1 != full  & !(left = true)) -> 0.5:(l_1'=15) + 0.5:(l_1'=17);
+	[] l_1=14 & (!(cp_1 != full) &   left = true ) -> 0.5:(l_1'=16) + 0.5:(l_1'=17);
+	[] l_1=15 &   left = true  -> (l_1'=10);
+	[] l_1=15 & !(left = true) -> (l_1'=17);
+	[] l_1=16 &   cp_1 != full  -> (l_1'=10);
+	[] l_1=16 & !(cp_1 != full) -> (l_1'=17);
 
-	[] s_1=14 & !(cp_1 != full) & !(left = true) -> (s_1'=17);
-	[] s_1=17 -> (s_1'=0) & (left'=false);
+	[] l_1=14 & !(cp_1 != full) & !(left = true) -> (l_1'=17);
+	[] l_1=17 -> (l_1'=0) & (left'=false);
 
 endmodule
 
 
 module process_2 = process_1 [
 	me_1 =me_2,
-	s_1  =s_2,
+	l_1  =l_2,
 	cp_1 =cp_2
 ] endmodule
