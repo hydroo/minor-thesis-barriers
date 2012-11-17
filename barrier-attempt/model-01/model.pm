cd1cbe379a8883d640b9757046912e82c2ecb9a4
Ronny Brendel <ronnybrendel@gmail.com>
2012-11-17 15:32:11 +0100

    fixed bug in the model

--- barrier-attempt/model-01/model.pm
+++ barrier-attempt/model-01/model.pm
@@ -6,6 +6,7 @@ const me_2 = 2;
 const me_bit_1 = 1; //2^0
 const me_bit_2 = 2; //2^1
 
+const empty = 0;
 const full = me_bit_1 + me_bit_2;
 
 const min_invalid = -1;
@@ -35,7 +36,7 @@ module process_1
 	l_1 : [0..17] init 0;
 	cp_1 : [min_invalid..max_invalid] init 0;
 
-	[] sched=me_1 & l_1=0 -> (l_1'=1) & (entry'=1);
+	[] sched=me_1 & l_1=0 -> (l_1'=1) & (entry'=empty);
 
 	[] sched=me_1 & l_1=1 -> (l_1'=2) & (cp_1'=entry);
 
@@ -60,7 +61,7 @@ module process_1
 
 	[] sched=me_1 & l_1=8 -> (l_1'=9) & (left'=true);
 
-	[] sched=me_1 & l_1=9 -> (l_1'=10) & (exit'=1);
+	[] sched=me_1 & l_1=9 -> (l_1'=10) & (exit'=empty);
 
 	[] sched=me_1 & l_1=10 -> (l_1'=11) & (cp_1'=exit);
 
