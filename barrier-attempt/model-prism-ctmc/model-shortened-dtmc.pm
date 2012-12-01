f1a432e5027205fe0c51a21b9a9c83d391a108ee
Ronny Brendel <ronnybrendel@gmail.com>
2012-12-01 14:09:45 +0100

    shorten mdp 3:  * number locations newly  * remove outdated things  * mini refactoring

--- barrier-attempt/model-prism-ctmc/model-shortened-dtmc.pm
+++ barrier-attempt/model-prism-ctmc/model-shortened-dtmc.pm
@@ -11,7 +11,7 @@ const me_bit_3 = 4;
 const empty = 0;
 const full = me_bit_1 + me_bit_2 + me_bit_3;
 
-const min_invalid = -1;
+const min_invalid = empty - 1;
 const max_invalid = full + 1;
 
 
@@ -21,32 +21,28 @@ global left : bool init false;
 
 
 module process_1
-	l_1 : [0..17] init 0;
+	l_1 : [0..5] init 0;
 	cp_1 : [min_invalid..max_invalid] init empty;
 
-	[] l_1=0 -> (l_1'=1) & (entry'=empty) & (left'=false);
-
-	[] l_1=1 -> (l_1'=2) & (cp_1'=entry);
+	[] l_1=0 -> (l_1'=1) & (cp_1'=entry);
 
 	// emulate bit manipulation: (cp_1&me) != 0
-	[] l_1=2 & mod(floor(cp_1/me_bit_1),2)=1 -> (l_1'=5);
+	[] l_1=1 & mod(floor(cp_1/me_bit_1),2)=1 -> (l_1'=2);
 	// emulate bit manipulation: (cp_1&me) == 0
-	[] l_1=2 & mod(floor(cp_1/me_bit_1),2)=0 ->  (l_1'=5) & (entry'=min(max_invalid,max(cp_1+me_bit_1, min_invalid)));
+	[] l_1=1 & mod(floor(cp_1/me_bit_1),2)=0 ->  (l_1'=2) & (entry'=min(max_invalid,max(cp_1+me_bit_1, min_invalid)));
 
 	// diamond/nondeterminism for cp_1 != full & left = false
-	[] l_1=5 & (  cp_1 != full & left = false ) -> (l_1'=1);
-	[] l_1=5 & (!(cp_1 != full & left = false)) -> (l_1'=8);
+	[] l_1=2 & (  cp_1 != full & left = false ) -> (l_1'=0);
+	[] l_1=2 & (!(cp_1 != full & left = false)) -> (l_1'=3) & (exit'=empty) & (left'=true);
 
-	[] l_1=8 -> (l_1'=10) & (exit'=empty) & (left'=true);
+	[] l_1=3 -> (l_1'=4) & (cp_1'=exit);
 
-	[] l_1=10 -> (l_1'=11) & (cp_1'=exit);
-
-	[] l_1=11 & mod(floor(cp_1/me_bit_1),2)=1 -> (l_1'=14);
-	[] l_1=11 & mod(floor(cp_1/me_bit_1),2)=0 -> (l_1'=14) & (exit'=min(max_invalid,max(cp_1+me_bit_1, min_invalid)));
+	[] l_1=4 & mod(floor(cp_1/me_bit_1),2)=1 -> (l_1'=5);
+	[] l_1=4 & mod(floor(cp_1/me_bit_1),2)=0 -> (l_1'=5) & (exit'=min(max_invalid,max(cp_1+me_bit_1, min_invalid)));
 
 	// diamond/nondeterminism for cp_1 != full & left = true
-	[] l_1=14 & (  cp_1 != full & left = true ) -> (l_1'=10);
-	[] l_1=14 & (!(cp_1 != full & left = true)) -> (l_1'=0);
+	[] l_1=5 & (  cp_1 != full & left = true ) -> (l_1'=3);
+	[] l_1=5 & (!(cp_1 != full & left = true)) -> (l_1'=0) & (entry'=empty) & (left'=false);
 
 endmodule
 
@@ -65,31 +61,3 @@ module process_3 = process_1 [
 	cp_1     =cp_3
 ] endmodule
 
-
-label "invalid_barrier" =
-	entry=min_invalid | entry=max_invalid |
-	cp_1=min_invalid  | cp_1=max_invalid |
-	cp_2=min_invalid  | cp_2=max_invalid |
-	cp_3=min_invalid  | cp_3=max_invalid |
-	exit=min_invalid  | exit=max_invalid;
-
-
-formula p1_before        = l_1 = 0 | l_1 = 17;
-formula p1_entry_barrier = l_1 >= 1 & l_1 <= 7;
-formula p1_between       = l_1 = 8 | l_1 = 9;
-formula p1_exit_barrier  = l_1 >= 10 & l_1 <= 16;
-
-formula p2_before        = l_2 = 0 | l_2 = 17;
-formula p2_entry_barrier = l_2 >= 1 & l_2 <= 7;
-formula p2_between       = l_2 = 8 | l_2 = 9;
-formula p2_exit_barrier  = l_2 >= 10 & l_2 <= 16;
-
-formula p3_before        = l_3 = 0 | l_3 = 17;
-formula p3_entry_barrier = l_3 >= 1 & l_3 <= 7;
-formula p3_between       = l_3 = 8 | l_3 = 9;
-formula p3_exit_barrier  = l_3 >= 10 & l_3 <= 16;
-
-label "invalid_locations" =
-	(p1_before & p2_between) | (p2_before & p1_between) |
-	(p2_before & p3_between) | (p3_before & p2_between) |
-	(p1_before & p3_between) | (p3_before & p1_between);
