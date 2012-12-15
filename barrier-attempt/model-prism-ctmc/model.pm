926cd61d9630a1633632293fe652570b47c79139
Ronny Brendel <ronnybrendel@gmail.com>
2012-12-15 13:22:38 +0100

    split the two writes into two locations/ two transitions

--- barrier-attempt/model-prism-ctmc/model.pm
+++ barrier-attempt/model-prism-ctmc/model.pm
@@ -30,7 +30,7 @@ formula write      = base_rate / 100;
 
 
 module process_1
-	l_1 : [0..7] init 0;
+	l_1 : [0..9] init 0;
 	cp_1 : [min_invalid..max_invalid] init empty;
 
 	[] l_1=0 -> read : (l_1'=1) & (cp_1'=entry);
@@ -39,22 +39,25 @@ module process_1
 	[] l_1=1 & mod(floor(cp_1/me_bit_1),2)=0 -> write : (l_1'=2) & (entry'=min(max_invalid,max(cp_1+me_bit_1, min_invalid)));
 
 	[] l_1=2 & (  cp_1 != full & left = false ) -> tick : (l_1'=0);
-	[] l_1=2 & (!(cp_1 != full & left = false)) -> write : (l_1'=3) & (exit'=empty) & (left'=true);
+	[] l_1=2 & (!(cp_1 != full & left = false)) -> write : (l_1'=3) & (exit'=empty);
+	[] l_1=3 -> write : (l_1'=4) & (left'=true);
 
 
-	[] l_1=3 -> work : (l_1'=4);
+	[] l_1=4 -> work : (l_1'=5);
 
 
-	[] l_1=4 -> read : (l_1'=5) & (cp_1'=exit);
+	[] l_1=5 -> read : (l_1'=6) & (cp_1'=exit);
 
-	[] l_1=5 & mod(floor(cp_1/me_bit_1),2)=1 -> tick : (l_1'=6);
-	[] l_1=5 & mod(floor(cp_1/me_bit_1),2)=0 -> write : (l_1'=6) & (exit'=min(max_invalid,max(cp_1+me_bit_1, min_invalid)));
+	[] l_1=6 & mod(floor(cp_1/me_bit_1),2)=1 -> tick : (l_1'=7);
+	[] l_1=6 & mod(floor(cp_1/me_bit_1),2)=0 -> write : (l_1'=7) & (exit'=min(max_invalid,max(cp_1+me_bit_1, min_invalid)));
 
-	[] l_1=6 & (  cp_1 != full & left = true ) -> tick : (l_1'=4);
-	[] l_1=6 & (!(cp_1 != full & left = true)) -> write : (l_1'=7) & (entry'=empty) & (left'=false);
+	[] l_1=7 & (  cp_1 != full & left = true ) -> tick : (l_1'=4);
+	[] l_1=7 & (!(cp_1 != full & left = true)) -> write : (l_1'=8) & (entry'=empty) & (left'=false);
+	[] l_1=7 & (!(cp_1 != full & left = true)) -> write : (l_1'=8) & (entry'=empty);
+	[] l_1=8 -> write : (l_1'=9) & (left'=false);
 
 
-	[] l_1=7 -> work : (l_1'=0);
+	[] l_1=9 -> work : (l_1'=0);
 
 endmodule
 
