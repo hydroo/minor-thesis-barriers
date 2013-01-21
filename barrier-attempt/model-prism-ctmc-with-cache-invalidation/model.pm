479a42ff6f0ad70932206848c2532b26b945cdd2
Ronny Brendel <ronnybrendel@gmail.com>
2013-01-21 16:09:22 +0100

    make left a pseudo global variable (by using sync operations) to save some states

--- barrier-attempt/model-prism-ctmc-with-cache-invalidation/model.pm
+++ barrier-attempt/model-prism-ctmc-with-cache-invalidation/model.pm
@@ -10,6 +10,7 @@ const me_bit_3 = 4; //2^2
 
 const empty = 0;
 const full = me_bit_1 + me_bit_2 + me_bit_3;
+//const full = me_bit_1 + me_bit_2;
 
 const min_invalid = empty - 1;
 const max_invalid = full + 1;
@@ -20,7 +21,6 @@ const invalid = 2;
 
 global entry : [min_invalid..max_invalid] init empty;
 global exit : [min_invalid..max_invalid] init empty;
-global left : bool init false;
 
 
 
@@ -37,6 +37,7 @@ module process_1
 	l_1 : [0..17] init 0;
 	cp_1 : [min_invalid..max_invalid] init empty;
 	mesi_1 : [0..2] init invalid;
+	left_1 : bool init false;
 
 	//process
 	[read_1]  l_1=0 & mesi_1 =invalid -> read : (l_1'=1) & (cp_1'=entry) & (mesi_1'=shared);
@@ -47,16 +48,17 @@ module process_1
 	[write_1] l_1=1 & mesi_1 =modified & mod(floor(cp_1/me_bit_1),2)=0 -> tick  : (l_1'=2);
 	[]        l_1=2 -> micro : (l_1'=3) & (entry'=min(max_invalid,max(cp_1+me_bit_1, min_invalid)));
 
-	[read_1]  l_1=3 & mesi_1 =invalid & (  cp_1 != full & left = false ) -> read : (l_1'=0) & (mesi_1'=shared);
-	[read_1]  l_1=3 & mesi_1!=invalid & (  cp_1 != full & left = false ) -> tick : (l_1'=0);
-	[read_1]  l_1=3 & mesi_1 =invalid & (!(cp_1 != full & left = false)) -> read : (l_1'=4) & (mesi_1'=shared);
-	[read_1]  l_1=3 & mesi_1!=invalid & (!(cp_1 != full & left = false)) -> tick : (l_1'=4);
+	[read_1]  l_1=3 & mesi_1 =invalid & (  cp_1 != full & left_1 = false ) -> read : (l_1'=0) & (mesi_1'=shared);
+	[read_1]  l_1=3 & mesi_1!=invalid & (  cp_1 != full & left_1 = false ) -> tick : (l_1'=0);
+	[read_1]  l_1=3 & mesi_1 =invalid & (!(cp_1 != full & left_1 = false)) -> read : (l_1'=4) & (mesi_1'=shared);
+	[read_1]  l_1=3 & mesi_1!=invalid & (!(cp_1 != full & left_1 = false)) -> tick : (l_1'=4);
 	[write_1] l_1=4 & mesi_1!=modified -> write : (l_1'=5) & (mesi_1'=modified);
 	[write_1] l_1=4 & mesi_1 =modified -> tick  : (l_1'=5);
 	[]        l_1=5 -> micro : (l_1'=6) & (exit'=empty);
-	[write_1] l_1=6 & mesi_1!=modified -> write : (l_1'=7) & (mesi_1'=modified);
-	[write_1] l_1=6 & mesi_1 =modified -> tick  : (l_1'=7);
-	[]        l_1=7 -> micro : (l_1'=8) & (left'=true);
+	[set_left_true_23] l_1=6 & mesi_1!=modified -> write : (left_1'=true) & (l_1'=8) & (mesi_1'=modified);
+	[set_left_true_23] l_1=6 & mesi_1 =modified -> tick  : (left_1'=true) & (l_1'=8);
+//	[set_left_true_2]  l_1=6 & mesi_1!=modified -> write : (left_1'=true) & (l_1'=8) & (mesi_1'=modified);
+//	[set_left_true_2]  l_1=6 & mesi_1 =modified -> tick  : (left_1'=true) & (l_1'=8);
 
 
 	[]        l_1=8 -> work : (l_1'=9);
@@ -70,16 +72,17 @@ module process_1
 	[write_1] l_1=10 & mesi_1 =modified & mod(floor(cp_1/me_bit_1),2)=0 -> tick  : (l_1'=11);
 	[]        l_1=11 -> micro : (l_1'=12) & (exit'=min(max_invalid,max(cp_1+me_bit_1, min_invalid)));
 
-	[read_1]  l_1=12 & mesi_1 =invalid & (  cp_1 != full & left = true ) -> read : (l_1'=9) & (mesi_1'=shared);
-	[read_1]  l_1=12 & mesi_1!=invalid & (  cp_1 != full & left = true ) -> tick : (l_1'=9);
-	[read_1]  l_1=12 & mesi_1 =invalid & (!(cp_1 != full & left = true)) -> read : (l_1'=13) & (mesi_1'=shared);
-	[read_1]  l_1=12 & mesi_1!=invalid & (!(cp_1 != full & left = true)) -> tick : (l_1'=13);
+	[read_1]  l_1=12 & mesi_1 =invalid & (  cp_1 != full & left_1 = true ) -> read : (l_1'=9) & (mesi_1'=shared);
+	[read_1]  l_1=12 & mesi_1!=invalid & (  cp_1 != full & left_1 = true ) -> tick : (l_1'=9);
+	[read_1]  l_1=12 & mesi_1 =invalid & (!(cp_1 != full & left_1 = true)) -> read : (l_1'=13) & (mesi_1'=shared);
+	[read_1]  l_1=12 & mesi_1!=invalid & (!(cp_1 != full & left_1 = true)) -> tick : (l_1'=13);
 	[write_1] l_1=13 & mesi_1!=modified -> write : (l_1'=14) & (mesi_1'=modified);
 	[write_1] l_1=13 & mesi_1 =modified -> tick  : (l_1'=14);
 	[]        l_1=14 -> micro : (l_1'=15) & (entry'=empty);
-	[write_1] l_1=15 & mesi_1!=modified -> write : (l_1'=16) & (mesi_1'=modified);
-	[write_1] l_1=15 & mesi_1 =modified -> tick : (l_1'=16);
-	[]        l_1=16 -> micro : (l_1'=17) & (left'=false);
+	[set_left_false_23] l_1=15 & mesi_1!=modified -> write : (left_1'=false) & (l_1'=17) & (mesi_1'=modified);
+	[set_left_false_23] l_1=15 & mesi_1 =modified -> tick  : (left_1'=false) & (l_1'=17);
+//	[set_left_false_2]  l_1=15 & mesi_1!=modified -> write : (left_1'=false) & (l_1'=17) & (mesi_1'=modified);
+//	[set_left_false_2]  l_1=15 & mesi_1 =modified -> tick  : (left_1'=false) & (l_1'=17);
 
 
 	[]        l_1=17 -> work : (l_1'=0);
@@ -97,6 +100,13 @@ module process_1
 	[read_2]  mesi_1=modified -> (mesi_1'=shared);
 	[read_3]  mesi_1=modified -> (mesi_1'=shared);
 
+	[set_left_false_12] true -> (left_1'=false) & (mesi_1'=invalid);
+	[set_left_false_13] true -> (left_1'=false) & (mesi_1'=invalid);
+	[set_left_true_12]  true -> (left_1'=true)  & (mesi_1'=invalid);
+	[set_left_true_13]  true -> (left_1'=true)  & (mesi_1'=invalid);
+//	[set_left_false_1]  true -> (left_1'=false) & (mesi_1'=invalid);
+//	[set_left_true_1]   true -> (left_1'=true)  & (mesi_1'=invalid);
+
 endmodule
 
 module process_2 = process_1 [
@@ -105,10 +115,21 @@ module process_2 = process_1 [
 	l_1      =l_2,
 	cp_1     =cp_2,
 	mesi_1   =mesi_2,
+	left_1   =left_2,
 	read_1   =read_2,
 	read_2   =read_1,
 	write_1  =write_2,
-	write_2  =write_1
+	write_2  =write_1,
+	set_left_false_12 = set_left_false_12,
+	set_left_false_13 = set_left_false_23,
+	set_left_false_23 = set_left_false_13,
+	set_left_true_12  = set_left_true_12,
+	set_left_true_13  = set_left_true_23,
+	set_left_true_23  = set_left_true_13
+//	set_left_false_1 = set_left_false_2,
+//	set_left_false_2 = set_left_false_1,
+//	set_left_true_1  = set_left_true_2,
+//	set_left_true_2  = set_left_true_1
 ] endmodule
 
 module process_3 = process_1 [
@@ -117,10 +138,17 @@ module process_3 = process_1 [
 	l_1      =l_3,
 	cp_1     =cp_3,
 	mesi_1   =mesi_3,
+	left_1   =left_3,
 	read_1   =read_3,
 	read_3   =read_1,
 	write_1  =write_3,
-	write_3  =write_1
+	write_3  =write_1,
+	set_left_false_12 = set_left_false_13,
+	set_left_false_13 = set_left_false_23,
+	set_left_false_23 = set_left_false_12,
+	set_left_true_12  = set_left_true_13,
+	set_left_true_13  = set_left_true_23,
+	set_left_true_23  = set_left_true_12
 ] endmodule
 
 
