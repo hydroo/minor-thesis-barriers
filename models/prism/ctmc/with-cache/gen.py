995f396db88f8693912a884b15e4bacfc3d55ec8
Ronny <ronnybrendel@gmail.com>
2013-04-20 15:22:28 +0200

    big commit
     * refactoring
      * left_ -> left__ for better optical alignment
      * exit_ -> exit__
      * change everyProcessButMyself
    
     * cache
      * remove extra case for allAreInvalid. default is now modified on cache process 1
    
     * correctness queries
      * implement complex lab queries
      * fix them (G != F errors were in there)
      * add some new

--- models/prism/ctmc/with-cache/gen.py
+++ models/prism/ctmc/with-cache/gen.py
@@ -74,20 +74,20 @@ def generateProcess(p, processCount) :
 
 	s = ""
 
-	s += "module process_" + str(p) + "\n"
+	s += "module process_#\n"
 
 	empty = str(0)
 	full = str((2**processCount)-1)
 	possibleValues = [str(i) for i in range(int(empty), int(full)+1)]
 
-	s += generateLocalVariable("l_", "[0..11]", "0")
-	s += generateLocalVariable("cp_", "[" + empty + ".." + full + "]", empty)
-	s += generateLocalVariable("exit_", "[" + empty + ".."+ full + "]", empty)
-	s += generateLocalVariable("entry_", "[" + empty + ".."+ full + "]", empty)
-	s += generateLocalVariable("left_", "bool", "false");
+	s += "\tl_# : [0..11] init 0;\n"
+	s += "\tcp_# : [empty..full] init empty;\n"
+	s += "\tentry_# : [empty..full] init empty;\n"
+	s += "\tleft__# : bool init false;\n"
+	s += "\texit__# : [empty..full] init empty;\n"
 	s += "\n"
 
-	others = everyProcessButMyself(p, processCount, "")
+	others = "".join([ str(i) for i in everyProcessButMyself(p, processCount)])
 
 	s += "\t[read_#] l_#=0 -> (l_#'=1) & (cp_#'=entry_#);\n"
 	s += "\n"
@@ -97,12 +97,12 @@ def generateProcess(p, processCount) :
 		s += "\t[set_entry_to_" + value + "_at_" + others + "] l_#=1 & mod(floor(cp_#/me_bit_#),2)=0 & cp_#=" + value + "-me_bit_# -> (l_#'=2) & (entry_#'=" + value + ") & (cp_#'=" + value + ");\n"
 	s += "\n"
 
-	s += "\t[read_#] l_#=2 & (  cp_# != full & left_# = false ) -> (l_#'=0);\n"
-	s += "\t[read_#] l_#=2 & (!(cp_# != full & left_# = false)) -> (l_#'=3);\n"
+	s += "\t[read_#] l_#=2 & (  cp_# != full & left__# = false ) -> (l_#'=0);\n"
+	s += "\t[read_#] l_#=2 & (!(cp_# != full & left__# = false)) -> (l_#'=3);\n"
 	s += "\n"
 
-	s += "\t[set_exit_to_0_at_" + others + "]    l_#=3 -> (l_#'=4) & (exit_#'=empty);\n"
-	s += "\t[set_left_to_true_at_" + others + "] l_#=4 -> (l_#'=5) & (left_#'=true);\n"
+	s += "\t[set_exit__to_0_at_" + others + "]    l_#=3 -> (l_#'=4) & (exit__#'=empty);\n"
+	s += "\t[set_left__to_true_at_" + others + "] l_#=4 -> (l_#'=5) & (left__#'=true);\n"
 
 	s += "\n"
 	s += "\n"
@@ -112,20 +112,20 @@ def generateProcess(p, processCount) :
 	s += "\n"
 	s += "\n"
 
-	s += "\t[read_#] l_#=6 -> (l_#'=7) & (cp_#'=exit_#);\n"
+	s += "\t[read_#] l_#=6 -> (l_#'=7) & (cp_#'=exit__#);\n"
 	s += "\n"
 
 	s += "\t[] l_#=7 & mod(floor(cp_#/me_bit_#),2)=1 -> (l_#'=8);\n"
 	for value in possibleValues:
-		s += "\t[set_exit_to_" + value + "_at_" + others + "] l_#=7 & mod(floor(cp_#/me_bit_#),2)=0 & cp_#=" + value + "-me_bit_# -> (l_#'=8) & (exit_#'=" + value + ") & (cp_#'=" + value + ");\n"
+		s += "\t[set_exit__to_" + value + "_at_" + others + "] l_#=7 & mod(floor(cp_#/me_bit_#),2)=0 & cp_#=" + value + "-me_bit_# -> (l_#'=8) & (exit__#'=" + value + ") & (cp_#'=" + value + ");\n"
 	s += "\n"
 
-	s += "\t[read_#] l_#=8 & (  cp_# != full & left_# = true ) -> (l_#'=6);\n"
-	s += "\t[read_#] l_#=8 & (!(cp_# != full & left_# = true)) -> (l_#'=9);\n"
+	s += "\t[read_#] l_#=8 & (  cp_# != full & left__# = true ) -> (l_#'=6);\n"
+	s += "\t[read_#] l_#=8 & (!(cp_# != full & left__# = true)) -> (l_#'=9);\n"
 	s += "\n"
 
 	s += "\t[set_entry_to_0_at_" + others + "]    l_#=9  -> (l_#'=10) & (entry_#'=empty);\n"
-	s += "\t[set_left_to_false_at_" + others + "] l_#=10 -> (l_#'=11) & (left_#'=false);\n"
+	s += "\t[set_left__to_false_at_" + others + "] l_#=10 -> (l_#'=11) & (left__#'=false);\n"
 
 	s += "\n"
 	s += "\n"
@@ -135,21 +135,18 @@ def generateProcess(p, processCount) :
 	s += "\n"
 	s += "\n"
 	s += generateSyncTransitionsForLocalVariables(p, processCount, "entry_", possibleValues)
-	s += generateSyncTransitionsForLocalVariables(p, processCount, "exit_", possibleValues)
-	s += generateSyncTransitionsForLocalVariables(p, processCount, "left_", ["false", "true"])
+	s += generateSyncTransitionsForLocalVariables(p, processCount, "exit__", possibleValues)
+	s += generateSyncTransitionsForLocalVariables(p, processCount, "left__", ["false", "true"])
 
 	s += "endmodule\n"
 
 	return s.replace('#', str(p))
 
-def generateLocalVariable(prefix, dataType, init) :
-	return "\t" + prefix + "#" + " : " + dataType + " init " + init + ";\n"
-
 def generateSyncTransitionsForLocalVariables(p, processCount, prefix, l) :
 	s = ""
 	for value in l :
 		for forWhom in forMe(p, processCount) :
-			s += "\t[set_" + prefix + "to_" + value + "_at_" + forWhom + "] true -> (" + prefix + "#'="+value + ");\n"
+			s += "\t[set_" + prefix + "to_" + value + "_at_" + forWhom + "] true -> (" + prefix + "#'=" + value + ");\n"
 	return s
 
 def generateRewards() :
@@ -171,41 +168,37 @@ def generateCache(processCount) :
 	full = str((2**processCount)-1)
 	possibleValues = [str(i) for i in range(int(empty), int(full)+1)]
 
-	s += "const int allAreInvalid     = 0;\n"
-	s += "const int someoneIsModified = 1;\n"
-	s += "const int someoneIsShared   = 2;\n"
+	s += "const int someoneIsModified = 0;\n"
+	s += "const int someoneIsShared   = 1;\n"
 	s += "\n"
 
 
 	s += "module cache\n"
 
-	s += "\tstate_c : [allAreInvalid..someoneIsShared] init allAreInvalid;\n"
-	s += "\twho_c   : [empty..full] init empty;\n"
+	s += "\t// we exclude the case that all are invalid and say the default is that the cache line copy on process 1 is modified\n"
+	s += "\tstate_c : [someoneIsModified..someoneIsShared] init someoneIsModified;\n"
+	s += "\twho_c   : [empty..full] init me_bit_1;\n"
 	s += "\n"
 
 	for p in range(1,processCount+1) :
 		bit = 2**(p-1)
-		others = everyProcessButMyself (p, processCount, '')
-		for variable in ["entry", "exit"] :
+		others = "".join([ str(i) for i in everyProcessButMyself (p, processCount)])
+		for variable in ["entry", "exit_"] :
 			for value in possibleValues :
-				s += "\t[set_" + variable + "_to_" + value + "_at_" + others + "]  (state_c=someoneIsModified & who_c=$) -> tick : true;\n"
-				s += "\t[set_" + variable + "_to_" + value + "_at_" + others + "] !(state_c=someoneIsModified & who_c=$) -> write : (state_c'=someoneIsModified) & (who_c'=$);\n"
+				s += "\t[set_" + variable + "_to_" + value + "_at_" + others + "]  (state_c=someoneIsModified & who_c=me_bit_#) -> tick : true;\n"
+				s += "\t[set_" + variable + "_to_" + value + "_at_" + others + "] !(state_c=someoneIsModified & who_c=me_bit_#) -> write : (state_c'=someoneIsModified) & (who_c'=me_bit_#);\n"
 
 		for value in ["false", "true"] :
-			s += "\t[set_left_to_" + value + "_at_" + others + "]  (state_c=someoneIsModified & who_c=$) -> tick : true;\n"
-			s += "\t[set_left_to_" + value + "_at_" + others + "] !(state_c=someoneIsModified & who_c=$) -> write : (state_c'=someoneIsModified) & (who_c'=$);\n"
+			s += "\t[set_left__to_" + value + "_at_" + others + "]  (state_c=someoneIsModified & who_c=me_bit_#) -> tick : true;\n"
+			s += "\t[set_left__to_" + value + "_at_" + others + "] !(state_c=someoneIsModified & who_c=me_bit_#) -> write : (state_c'=someoneIsModified) & (who_c'=me_bit_#);\n"
 
-		s += "\t[read_#] state_c=allAreInvalid -> read : (state_c'=someoneIsShared) & (who_c'=$);\n"
-		s += "\t[read_#] state_c=someoneIsModified & who_c=$ -> tick : true;\n"
-		s += "\t[read_#] state_c=someoneIsModified & who_c!=$ -> read : (state_c'=someoneIsShared) & (who_c'=min(full,max(who_c+$, empty)));\n"
-		s += "\t[read_#] state_c=someoneIsShared & mod(floor(who_c/$),2)=1 -> tick : true;\n"
-		s += "\t[read_#] state_c=someoneIsShared & mod(floor(who_c/$),2)=0 -> read : (who_c'=min(full,max(who_c+$, empty)));\n"
-
+		s += "\t[read_#] state_c=someoneIsModified & who_c=me_bit_# -> tick : true;\n"
+		s += "\t[read_#] state_c=someoneIsModified & who_c!=me_bit_# -> read : (state_c'=someoneIsShared) & (who_c'=min(full,max(who_c+me_bit_#, empty)));\n"
+		s += "\t[read_#] state_c=someoneIsShared & mod(floor(who_c/me_bit_#),2)=1 -> tick : true;\n"
+		s += "\t[read_#] state_c=someoneIsShared & mod(floor(who_c/me_bit_#),2)=0 -> read : (who_c'=min(full,max(who_c+me_bit_#, empty)));\n"
 		s += "\n"
 
-		s = s.replace('#', str(p)).replace('$', str(bit))
-
-	# todo
+		s = s.replace('#', str(p))
 
 	s += "endmodule\n"
 	s += "\n"
@@ -219,26 +212,83 @@ def generateCacheLabels() :
 
 	for p in range(1, processCount+1) :
 		bit = 2**(p-1)
-		s += "label \"invalid_#\"  = (state_c=allAreInvalid) | ((state_c=someoneIsModified | state_c=someoneIsShared) & mod(floor(who_c/$),2)=0);\n"
-		s += "label \"modified_#\" = (state_c=someoneIsModified & mod(floor(who_c/$),2)=1);\n"
-		s += "label \"shared_#\"   = (state_c=someoneIsShared   & mod(floor(who_c/$),2)=1);\n"
-		s = s.replace('#', str(p)).replace('$', str(bit))
+		s += "label \"invalid_#\"  = ((state_c=someoneIsModified | state_c=someoneIsShared) & mod(floor(who_c/me_bit_#),2)=0);\n"
+		s += "label \"modified_#\" = (state_c=someoneIsModified & who_c=me_bit_#);\n"
+		s += "label \"shared_#\"   = (state_c=someoneIsShared   & mod(floor(who_c/me_bit_#),2)=1);\n"
+		s = s.replace('#', str(p))
 
 	return s
 
 # ### correctness props ### ###################################################
-def generateCorrectnessProperties(processCount, workTicks, readTicks, writeTicks) :
-	return ""
+def generateCorrectnessProperties(processCount) :
+
+	s = ""
+
+	s += "const double error=1.0E-6;\n"
+	s += "\n"
+
+	s += "// synchronized local variables have the same values\n"
+	for variable in ["entry_", "exit__", "left__"]:
+		for i in range(1, processCount) :
+			s += "P<=0 [F " + variable + str(i) + "  != " + variable + str(i+1) + "]\n"
+	s += "\n"
+
+
+	s += "// deadlock-freedom\n"
+	for i in range(1, processCount+1) :
+		s += "P>=1 [G F l_" + str(i) +"=11]\n"
+	s += "\n"
+
+	s += "// consistency of the barrier\n"
+	for p in range(1, processCount+1) :
+		s += "P<=0 [F (l_" + str(p) + "=3 & ("
+		for q in everyProcessButMyself(p, processCount) :
+			s += "l_" + str(q) + "=9|"
+		s += "false))]\n"
+	s += "\n"
+
+	s += "// ### cache properties ###\n"
+	s += "\n"
+
+	s += "// one and only one process can be in modified state at a time\n"
+	s += "P<=0 [F (state_c=someoneIsModified & !("
+	for p in range(1, processCount+1) :
+		bit = 2**(p-1)
+		s += "who_c=me_bit_" + str(p) + "|"
+	s += "false))];\n"
+	s += "\n"
+
+	s += "// if one cacheline is shared, at least one other is too\n"
+	for p in range(1, processCount+1) :
+		s += "P<=0 [F (\"shared_" + str(p) + "\" & !("
+		for q in everyProcessButMyself(p, processCount) :
+			s += "\"shared_" + str(q) + "\"|"
+		s += "false))]\n"
+	s += "\n"
+
+	s += "// every cache state can be reached\n"
+	for p in range(1, processCount+1) :
+		s += "P>=1 [F (\"modified_" + str(p) + "\")]\n"
+		s += "P>=1 [F (\"shared_" + str(p) + "\")]\n"
+		s += "P>=1 [F (\"invalid_" + str(p) + "\")]\n"
+	s += "\n"
+
+	s += "// steady state probs for cache states are equal for all processes\n"
+	for state in ["modified_", "shared_"]:
+		#invalid_ is implicitely correct if the others are
+		for p in range(1, processCount) :
+				s += "((S=? [\""+ state + str(p) + "\"] - S=? [\"" + state + str(p+1) + "\"]) <= error) | (error*-1 <= (S=? [\"" + state + str(p) + "\"] - S=? [\"" + state + str(p+1) + "\"]))\n"
+	s += "\n"
+
+	return s
 
 # ### helper ### ##############################################################
-def everyProcessButMyself (p, processCount, delimiter) :
-	s = ""
+def everyProcessButMyself (p, processCount) :
+	l = []
 	for j in range(1, processCount+1):
 		if p != j:
-			s += str(j)
-			if j < processCount :
-				s += delimiter
-	return s
+			l += [j]
+	return l
 
 def forMe(p, processCount) :
 	#me and all but one other
@@ -307,7 +357,7 @@ if __name__ == "__main__":
 
 	modelString = generateModel(processCount, workTicks, readTicks, writeTicks)
 
-	correctnessPropertiesString = generateCorrectnessProperties(processCount, workTicks, readTicks, writeTicks)
+	correctnessPropertiesString = generateCorrectnessProperties(processCount)
 
 	f = open(modelFileName, 'w')
 	f.write(modelString)
